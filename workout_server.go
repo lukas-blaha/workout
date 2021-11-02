fd51259f7b1672f0b29f5bc593a86f9005a8b76a
Lukas Blaha <lukas.blaha@seznam.cz>
2021-11-02 16:10:05 +0100

    Add client

--- workout_server.go
+++ workout_server.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"database/sql"
 	"fmt"
 	"log"
-	"os"
+	"net"
 	"strconv"
 	"strings"
 
@@ -36,7 +37,7 @@ func findEx(ex string) bool {
 	return false
 }
 
-func queryDb(db *sql.DB, ent *entry, prnt bool, date string, exercise string) {
+func queryDb(conn net.Conn, db *sql.DB, ent *entry, prnt bool, date string, exercise string) {
 	var condition string
 	if exercise == "all" {
 		condition = fmt.Sprintf(" where date = '%s'", date)
@@ -54,7 +55,7 @@ func queryDb(db *sql.DB, ent *entry, prnt bool, date string, exercise string) {
 		checkError(err)
 
 		if prnt {
-			fmt.Printf("%s: %d\n", ent.name, ent.count)
+			fmt.Fprintf(conn, "%s: %d\n", ent.name, ent.count)
 		}
 	}
 }
@@ -66,7 +67,7 @@ type entry struct {
 	count int
 }
 
-func main() {
+func manageRequest(conn net.Conn, args []string) {
 	dbconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 
@@ -74,30 +75,30 @@ func main() {
 	checkError(err)
 	defer db.Close()
 
-	if len(os.Args) > 2 {
-		opt := strings.ToLower(os.Args[1])
+	if len(args) >= 2 {
+		opt := strings.ToLower(args[0])
 		opt = strings.ReplaceAll(opt, "-", "")
 
 		if opt == "list" {
-			if len(os.Args) >= 3 {
-				exercise := strings.ToLower(os.Args[2])
+			if len(args) >= 2 {
+				exercise := strings.ToLower(args[1])
 				exercise = strings.ReplaceAll(exercise, "-", "")
 
 				if findEx(exercise) || exercise == "all" {
 					date := "today"
-					if len(os.Args) > 3 {
-						date = os.Args[3]
+					if len(args) > 2 {
+						date = args[2]
 					}
 					var ent entry
-					queryDb(db, &ent, true, date, exercise)
+					queryDb(conn, db, &ent, true, date, exercise)
 				}
 			}
 		} else if findEx(opt) {
-			if len(os.Args) > 2 {
-				count := os.Args[2]
+			if len(args) >= 2 {
+				count := args[1]
 				if string(count[0]) == "-" || string(count[0]) == "+" {
 					var ex entry
-					queryDb(db, &ex, false, "today", opt)
+					queryDb(conn, db, &ex, false, "today", opt)
 					if ex.id == 0 {
 						inp := `insert into "exercises"("date", "name", "count") values($1, $2, $3)`
 						_, err := db.Exec(inp, "today", opt, count)
@@ -116,14 +117,36 @@ func main() {
 					}
 
 				} else {
-					fmt.Printf("You have to use +/- new value\n%s\n",
+					fmt.Fprintf(conn, "You have to use +/- new value\n%s\n",
 						"Ex.: myworkout pushups +20")
 				}
 			}
 		} else {
-			fmt.Println("incorrect option")
+			fmt.Fprintln(conn, "incorrect option")
 		}
 	} else {
-		fmt.Println("You have to specify at least one option")
+		fmt.Fprintln(conn, "You have to specify at least one option")
+	}
+}
+
+func main() {
+	li, err := net.Listen("tcp", ":31564")
+	checkError(err)
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		checkError(err)
+
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		manageRequest(conn, strings.Split(scanner.Text(), " "))
+		break
 	}
 }
